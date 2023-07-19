0556ed2f733d41aef81f24e651ed205422f8ad95
Deepa612 <139889140+Deepa612@users.noreply.github.com>
2023-07-19 15:15:34 +0530

    Update WebServiceClient.java

--- microservice/src/main/java/com/telstra/codechallenge/ServiceImpl/WebServiceClient.java
+++ microservice/src/main/java/com/telstra/codechallenge/ServiceImpl/WebServiceClient.java
@@ -1,31 +1,24 @@
 package com.telstra.codechallenge.ServiceImpl;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.telstra.codechallenge.Constants.Constants;
-import com.telstra.codechallenge.DTO.Items;
 import com.telstra.codechallenge.DTO.UserInformation;
 import com.telstra.codechallenge.ExceptionHandling.ExceptionClass;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @Slf4j
 public class WebServiceClient {
-    @Value("${zero.followers.url}")
-    private String url;
     @Autowired
     RestTemplate restTemplate;
+    @Value("${zero.followers.url}")
+    private String url;
 
     /**
      * This method is used to call GitHub external api to find the
@@ -33,17 +26,14 @@ public class WebServiceClient {
      *
      * @return List<Items>
      */
-    public List<Items> getUsersData() {
+    public UserInformation getUsersData() {
         log.info("Inside WebServiceClient.getUsersInfo");
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        List<Items> items = new ArrayList<>();
+        UserInformation userInformation = new UserInformation();
         try {
             log.info("Inside try block to call external api");
-            Object[] userData = new Object[]{restTemplate.getForObject(url
-                    , Object.class)};
-            items = Arrays.asList(userData).stream().map(o -> mapper.convertValue(o, UserInformation.class)).map(
-                    UserInformation::getItems).collect(Collectors.toList()).get(0);
+            ResponseEntity<UserInformation> response = restTemplate.getForEntity(url
+                    , UserInformation.class);
+            userInformation = response.getBody();
             log.info("outside of try block");
         } catch (RuntimeException ex) {
             log.info("Inside catch block to call external api");
@@ -57,7 +47,7 @@ public class WebServiceClient {
             }
         }
         log.info("Outside WebServiceClient.getUsersInfo");
-        return items;
+        return userInformation;
     }
 }
 
