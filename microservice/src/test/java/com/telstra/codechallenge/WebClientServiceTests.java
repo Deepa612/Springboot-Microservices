59d4fc03c979121012a949982921ce83354b54cf
Deepa612 <139889140+Deepa612@users.noreply.github.com>
2023-07-19 15:20:01 +0530

    Update WebClientServiceTests.java

--- microservice/src/test/java/com/telstra/codechallenge/WebClientServiceTests.java
+++ microservice/src/test/java/com/telstra/codechallenge/WebClientServiceTests.java
@@ -10,19 +10,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -39,29 +41,28 @@ public class WebClientServiceTests {
 
     @Test
     public void getUsersInfo() {
-        Items items = new Items();
+        UserInformation userInformation = new UserInformation();
         List<Items> itemsList = new ArrayList<>();
+        Items items = new Items();
         items.setId(3);
         items.setLogin("mattetti");
         items.setHtml_url("https://github.com/mattetti");
         itemsList.add(items);
-        UserInformation userInformation = new UserInformation();
         userInformation.setItems(itemsList);
-
-        when(restTemplate.getForObject(url, Object.class)).thenReturn(userInformation);
-        List<Items> itemsList1 = webServiceClient.getUsersData();
-        assertNotNull(itemsList1);
+        Mockito.when(restTemplate.getForEntity(url, UserInformation.class)).thenReturn(ResponseEntity.of(Optional.of(userInformation)));
+        UserInformation userInformation1 = webServiceClient.getUsersData();
+        assertNotNull(userInformation1);
     }
 
     @Test()
     public void getUsersInfoInvalidParameterValueException() {
-        when(webServiceClient.getUsersData()).thenThrow(new ExceptionClass(Constants.ERROR_CODE3, Constants.Error_RESPONSE422, HttpStatus.UNPROCESSABLE_ENTITY));
+        Mockito.when(webServiceClient.getUsersData()).thenThrow(new ExceptionClass(Constants.ERROR_CODE3, Constants.Error_RESPONSE422, HttpStatus.UNPROCESSABLE_ENTITY));
 
     }
 
     @Test()
     public void getUsersInfoInvalidURLException() {
-        when(webServiceClient.getUsersData()).thenThrow(new ExceptionClass(Constants.ERROR_CODE1, Constants.Error_RESPONSE404, HttpStatus.NOT_FOUND));
+        Mockito.when(webServiceClient.getUsersData()).thenThrow(new ExceptionClass(Constants.ERROR_CODE1, Constants.Error_RESPONSE404, HttpStatus.NOT_FOUND));
 
     }
 
